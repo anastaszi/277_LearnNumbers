caf462ee34060375e29e96d1bfa9ca30647aa53c
Anastasia Zimina <anastasba@gmail.com>
2021-05-23 13:52:19 -0700

    Initial Commit

---
+++ LearnNumbers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnNumbers
+//
+//  Created by Anastasia Zimina on 5/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnNumbers/LearnNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnNumbersApp.swift
+//  LearnNumbers
+//
+//  Created by Anastasia Zimina on 5/23/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
